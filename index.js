ad061d01a71614c11d1b6c106699c91bfc897a60
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-23 12:55:03 -0800

    theoretically working but twitter is not right now

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var Twitter = require('node-twitter');
+// Set up hardware
+var tessel = require('tessel');
+var servo = require('servo-pca9685').use(tessel.port['A']);
 
+// Set up Twitter stream
+var Twitter = require('node-twitter');
 var twecrets = require('./secrets.js');
+var searchTerms = ['christmas'];
 
 // Authorize a stream client
 var twitterStreamClient = new Twitter.StreamClient(
@@ -10,11 +15,24 @@ var twitterStreamClient = new Twitter.StreamClient(
   twecrets.tokenSecret
 );
 
-// Start streaming anything containing these strings
-twitterStreamClient.start(['christmas']);
+// When the servo is ready
+servo.on('ready', function () {
+  // Start streaming anything containing these strings
+  twitterStreamClient.start(searchTerms);
+});
 
 // When we get something
 twitterStreamClient.on('tweet', function(tweet) {
   var thisTweet = tweet.text;
   console.log(thisTweet);
+  twitch();
 });
+
+function twitch () {
+  // Move forward a little
+  servo.move(1, 0.6);
+  setTimeout(function () {
+    // Stop
+    servo.move(1, 0.5);
+  }, 100);
+}
